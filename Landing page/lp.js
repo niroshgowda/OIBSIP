db3596a073d60d6a2fa7c54f5a83a48899e78873
niroshgowda <110534320+niroshgowda@users.noreply.github.com>
2024-01-02 20:08:44 +0530

    Add files via upload

---
+++ Landing page/lp.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything) {
+    document.querySelector('.Starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
